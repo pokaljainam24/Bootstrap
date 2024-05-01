f05c90b5d6594672e3562ae4de6e48dd914199c4
pokaljainam24 <pokaljainam@gmail.com>
2024-05-01 15:02:40 -0400

    done

---
+++ Traweling Web-page/script.js
@@ -0,0 +1,52 @@
+$("#counter").counter({
+    autoStart: true,
+    duration: 5000,
+    countFrom: 100,
+    countTo: 9313,
+    runOnce: false,
+    placeholder: "9,313",
+  
+    easing: "easeOutCubic",
+  
+  
+  });
+  
+  $("#counter1").counter({
+    autoStart: true,
+    duration: 1000,
+    countFrom: 100,
+    countTo: 8492,
+    runOnce: false,
+    placeholder: "8,492",
+  
+    easing: "easeOutCubic",
+  
+  });
+  
+  
+  $("#counter2").counter({
+    autoStart: true,
+    duration: 1000,
+    countFrom: 100,
+    countTo: 1000,
+    runOnce: false,
+    placeholder: "1000",
+  
+    easing: "easeOutCubic",
+  
+  
+  });
+  
+  $("#counter3").counter({
+    autoStart: true,
+    duration: 1000,
+    countFrom: 100,
+    countTo: 1200,
+    runOnce: false,
+    placeholder: "1200",
+  
+    easing: "easeOutCubic",
+  
+  
+  });
+  
